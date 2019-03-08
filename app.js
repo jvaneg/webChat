383587287ca4e3e08691f15eca54909c47492926
Joel <joel.vanegmond@ucalgary.ca>
2019-03-07 19:48:14 -0700

    did more

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const path = require("path");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const { User, Message } = require("./classes");
+const { User, Message } = require("./src/classes");
 
+// constants
 const PORT = 3333;
+
+const MessageType = {
+    NORMAL: 1,
+    NICKNAME: 2,
+    COLOUR: 3,
+};
+
+const NICK_COMMAND = "/nick";
+const COLOUR_COMMAND = "/nickcolor";
 
 
 // sets express to use the views folder for views
@@ -27,6 +37,7 @@ let activeUsers = {};
 let messageList = [];
 let userCount = 0;
 
+
 // --- routes ---
 
 // index route
@@ -42,11 +53,20 @@ connection
 Handles new connection and session
 */
 io.on("connection", (socket) => {
-    let user = new User(`anon${userCount++}`);
+    let userName = null;
+    do {
+        userName = `anon${userCount++}`;
+    } while(userName in allUsers);
+
+    let user = new User(userName);
     allUsers[user.name] = user;
-    activeUsers[user.name] = user;
     let joinTimestamp = Date.now();
-    console.log(`${joinTimestamp} - ${user.name} connected.`);
+
+    socket.emit("initialConnect", activeUsers, messageList, user);
+
+    activeUsers[user.name] = user;
+
+    console.log(`${new Date(joinTimestamp).toLocaleTimeString()} - ${user.name} connected.`);
     io.emit("userConnected", user, joinTimestamp);
 
     /*
@@ -56,7 +76,8 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
         delete activeUsers[user.name];
         let timestamp = Date.now();
-        console.log(`${timestamp} - ${user.name} disconnected.`);
+
+        console.log(`${new Date(timestamp).toLocaleTimeString()} - ${user.name} disconnected.`);
         io.emit("userDisconnected", user, timestamp);
     });
 
@@ -66,9 +87,158 @@ io.on("connection", (socket) => {
     When the server receives a message, broadcasts it to all users
     */
     socket.on("sendMessage", (message) => {
-        let newMessage = new Message(user, message);
-        messageList.push(newMessage);
-        console.log(`${newMessage.timestamp} - ${user.name}: ${newMessage.content}`);
-        io.emit("newMessage", newMessage);
+        messageType = getMessageType(message);
+        let result = null;
+
+        switch(messageType) {
+            case MessageType.NICKNAME:
+                result = processNickMessage(message, user);
+
+                if(!result[0]) {
+                    console.log(`${new Date(result[1].timestamp).toLocaleTimeString()} - ${result[1].oldName} changed name to ${result[1].newName}.`);
+                    io.emit("userNameChange", result[1].oldName, result[1].newName, result[1].timestamp);
+                }
+                else {
+                    console.log(`Error: ${result[1]}`);
+                    socket.emit("userNameChangeError", result[1]);
+                }
+
+                break;
+
+            case MessageType.COLOUR:
+                console.log("colour message");
+
+                result = processColourMessage(message, user);
+
+                if(!result[0]) {
+                    console.log(`${new Date(result[1].timestamp).toLocaleTimeString()} - ${result[1].user.name} changed colour from ${result[1].oldColour} to ${result[1].user.colour}.`);
+                    io.emit("userColourChange", result[1].user, result[1].oldColour, result[1].timestamp);
+                }
+                else {
+                    console.log(`Error: ${result[1]}`);
+                    socket.emit("userColourChangeError", result[1]);
+                }
+
+                break;
+
+            default:
+                let newMessage = new Message(user, message);
+                messageList.push(newMessage);
+
+                console.log(`${new Date(newMessage.timestamp).toLocaleTimeString()} - ${user.name}: ${newMessage.content}`);
+                io.emit("newMessage", newMessage);
+        }
     });
 });
+
+// --- helper functions ---
+
+/*
+getMessageType
+Determines the type of message
+*/
+function getMessageType(message) {
+    let splitMessage = message.split(" ", 2);
+    if(splitMessage[0] === NICK_COMMAND) {
+        return MessageType.NICKNAME;
+    }
+    else if(splitMessage[0] === COLOUR_COMMAND) {
+        return MessageType.COLOUR;
+    }
+    
+    return MessageType.NORMAL;
+}
+
+/*
+processNickMessage
+processes a nickname change event
+*/
+function processNickMessage(message, user) {
+    let error = false;
+    let errMessage = "";
+    let splitMessage = message.split(" ", 2);
+    let newNick = "";
+    let result = null;
+
+    if(splitMessage.length < 2) {
+        error = true;
+        errMessage = `${NICK_COMMAND} requires an argument.`;
+    } else {
+        newNick = message.substring(NICK_COMMAND.length).trim();
+
+        if(newNick === "") {
+            error = true;
+            errMessage = "nickname cannot be blank.";
+        } else if (newNick in allUsers) {
+            error = true;
+            errMessage = `${newNick} is already in use.`;
+        } else {
+            oldNick = user.name;
+
+            user.name = newNick;
+            allUsers[newNick] = user;
+            activeUsers[newNick] = user;
+
+            delete allUsers[oldNick];
+            delete activeUsers[oldNick];
+
+            let timestamp = Date.now();
+
+            result = {
+                oldName: oldNick,
+                newName: newNick,
+                timestamp: timestamp,
+            };
+        }
+    }
+
+    if(error) {
+        result = errMessage;
+    }
+
+    return [error, result];
+}
+
+/*
+processColourMessage
+process a colour change event
+*/
+function processColourMessage(message, user) {
+    let error = false;
+    let errMessage = "";
+    let splitMessage = message.split(" ", 2);
+    let newColour = "";
+    let result = null;
+    let colourCheck = /^[A-F0-9]{6}/i;
+
+    if(splitMessage.length < 2) {
+        error = true;
+        errMessage = `${COLOUR_COMMAND} requires an argument.`;
+    } else {
+        newColour = message.substring(COLOUR_COMMAND.length).trim();
+
+        if(newColour === "") {
+            error = true;
+            errMessage = "colour cannot be blank.";
+        } else if(!colourCheck.test(newColour.toUpperCase())) {
+            error = true;
+            errMessage = `${newColour} is not a valid colour`;
+        } else {
+            let oldColour = user.colour;
+            user.colour = `#${newColour}`;
+            let timestamp = Date.now();
+
+            result = {
+                user: user,
+                oldColour: oldColour,
+                timestamp: timestamp,
+            };
+        }
+    }
+
+    if(error) {
+        result = errMessage;
+    }
+
+    return [error, result];
+}
--- public/js/client.js
+++ public/js/client.js
@@ -3,32 +3,162 @@
 var socket = io();
 
 /*
-new message
+newMessage
 Handles receiving a new message from the server
 */
 socket.on("newMessage", (message) => {
-    console.log(`${message.timestamp} - ${message.user.name}: ${message.content}`);
-    $("#messages").append($("<li>").text(`${message.timestamp} - ${message.user.name}: ${message.content}`));
+    console.log(`${new Date(message.timestamp).toLocaleTimeString()} - ${message.user.name}: ${message.content}`);
+
+    let newMessageElem = $(document.createElement("li"))
+        .text(`${new Date(message.timestamp).toLocaleTimeString()} - ${message.user.name}: ${message.content}`);
+
+    $("#messages").append(newMessageElem);
 });
 
+/*
+userConnected
+Handles a user connected message from the server
+Adds a message indicacting the new connection to the chat
+Adds the user to the active users list
+*/
 socket.on("userConnected", (user, timestamp) => {
-    console.log(`${user.name} connected.`);
-    $("#messages").append($("<li>").text(`${timestamp} - ${user.name} connected.`));
+    console.log(`${new Date(timestamp).toLocaleTimeString()} - ${user.name} connected.`);
+
+    let newMessageElem = $(document.createElement("li"))
+        .text(`${new Date(timestamp).toLocaleTimeString()} - ${user.name} connected.`);
+    let newUserElem = $(document.createElement("li"))
+        .prop("id", user.name)
+        .text(user.name)
+        .css('color', user.colour);
+
+    $("#messages").append(newMessageElem);
+    $("#activeUsers").append(newUserElem);
 });
 
+/*
+userDisconnected
+Handles a user disconnecting message from the server
+Adds a message indicacting the disconnection to the chat
+Removes the user to the active users list
+*/
 socket.on("userDisconnected", (user, timestamp) => {
-    console.log(`${user.name} disconnected.`);
-    $("#messages").append($("<li>").text(`${timestamp} - ${user.name} disconnected.`));
+    console.log(`${new Date(timestamp).toLocaleTimeString()} - ${user.name} disconnected.`);
+
+    let newMessageElem = $(document.createElement("li"))
+        .text(`${new Date(timestamp).toLocaleTimeString()} - ${user.name} disconnected.`);
+
+    $("#messages").append(newMessageElem);
+    $(`#activeUsers > #${user.name}`).remove();
+});
+
+/*
+userNameChange
+Handles a user changed their name message from the server
+Adds a message indicacting the user's name change
+Changes the user's name in the active users list
+*/
+socket.on("userNameChange", (oldName, newName, timestamp) => {
+    console.log(`${new Date(timestamp).toLocaleTimeString()} - ${oldName} changed name to ${newName}.`);
+    
+    let newMessageElem = $(document.createElement("li"))
+        .text(`${new Date(timestamp).toLocaleTimeString()} - ${oldName} changed name to ${newName}.`);
+    let userElem = $(`#activeUsers > #${oldName}`);
+    
+    $("#messages").append(newMessageElem);
+    userElem
+        .prop("id", newName)
+        .text(newName);    
+});
+
+/*
+userNameChangeError
+Handles receiving an error from the server when trying to change name
+*/
+socket.on("userNameChangeError", (errMessage) => {
+    console.log(`Error: ${errMessage}`);
+
+    let newMessageElem = $(document.createElement("li"))
+        .text(`Error: ${errMessage}.`);
+    
+    $("#messages").append(newMessageElem);
+});
+
+/*
+userColourChange
+Handles a user changed their colour message from the server
+Adds a message indicacting the user's colour change
+Changes the user's colour in the active users list
+*/
+socket.on("userColourChange", (user, oldColour, timestamp) => {
+    console.log(`${new Date(timestamp).toLocaleTimeString()} - ${user.name} changed colour from to ${oldColour} to ${user.colour}.`);
+    
+    let newMessageElem = $(document.createElement("li"))
+        .text(`${new Date(timestamp).toLocaleTimeString()} - ${user.name} changed colour from to ${oldColour} to ${user.colour}.`);
+    let userElem = $(`#activeUsers > #${user.name}`);
+    
+    $("#messages").append(newMessageElem);
+    userElem
+        .css('color', user.colour);   
+});
+
+/*
+userColourChangeError
+Handles receiving an error from the server when trying to change colour
+*/
+socket.on("userColourChangeError", (errMessage) => {
+    console.log(`Error: ${errMessage}`);
+
+    let newMessageElem = $(document.createElement("li"))
+        .text(`Error: ${errMessage}.`);
+    
+    $("#messages").append(newMessageElem);
+});
+
+/*
+initialConnect
+Handles a user initally connecting to the server
+Adds the chat history and the list of active users
+Tells the user their name
+*/
+socket.on("initialConnect", (activeUsers, messageList, user) => {
+    //console.log(`${new Date(timestamp).toLocaleTimeString()} - ${user.name} changed colour from to ${oldColour} to ${user.colour}.`);
+    
+    // get own name
+    let myName = $("#name");
+    myName
+        .text(user.name)
+        .css('color', user.colour);
+
+    // add messages from history
+    let messageBlock = $("#messages");
+    for( let message of messageList) {
+        let newMessageElem = $(document.createElement("li"))
+            .text(`${new Date(message.timestamp).toLocaleTimeString()} - ${message.user.name}: ${message.content}`);
+
+        messageBlock.append(newMessageElem);
+    }
+
+    // add active user list
+    let activeUsersBlock = $("#activeUsers");
+    for( let activeUser of Object.values(activeUsers)) {
+        let newUserElem = $(document.createElement("li"))
+            .prop("id", activeUser.name)
+            .text(activeUser.name)
+            .css('color', activeUser.colour);
+
+        activeUsersBlock.append(newUserElem);
+    }
 });
 
 
 // jquery document.onready
 $(function () {
+
     // on submitting with the "Send" button
-    $('form').submit(function(e){
+    $('form').submit(function(e) {
         e.preventDefault(); // prevents page reloading
-        socket.emit('sendMessage', $('#m').val());
-        $('#m').val('');
+        socket.emit('sendMessage', $('#inputMsg').val());
+        $('#inputMsg').val('');
         return false;
     });
 });
